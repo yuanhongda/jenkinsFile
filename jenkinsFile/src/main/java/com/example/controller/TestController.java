c8b50df0a9f7913c7b959ad427ab3d19790d267b
abbott000 <abbott@mail.fameex.info>
2024-12-16 01:58:53 +0800

    测试获取覆盖率

--- jenkinsFile/src/main/java/com/example/controller/TestController.java
+++ jenkinsFile/src/main/java/com/example/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @GetMapping(value = "/say", name = "返回Hello World")
+    @GetMapping(value = "/toobit", name = "返回Hello World")
     public String hello(@RequestParam(value = "name", required = false) String name) {
         return String.format("Hello %s! ", name == null ? "World" : name);
     }
